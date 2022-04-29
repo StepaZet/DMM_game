89d5eddec114ec1814c682a48f0e314d85978154
Filipp <58597315+cfouf@users.noreply.github.com>
2022-04-29 18:00:11 +0500

    база готова

---
+++ Assets/MainGameScripts/GUIManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace MainSceneScripts
+{
+    public class GUIManager : MonoBehaviour
+    {
+    
+    }
+}
---
+++ Assets/StartMenuScripts/GUIManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StartMenuScripts
+{
+    public class GUIManager : MonoBehaviour
+    {
+    
+    
+    }
+}
