e4be682987d3628638111b2ecc29f31feaf39e3b
StepaZet <stepa.zet2@gmail.com>
2022-04-30 19:25:32 +0500

    Пока косячное движение

--- Assets/MainGameScripts/PlayableObjectsScripts/HumanDebug.cs
+++ Assets/MainGameScripts/PlayableObjectsScripts/HumanDebug.cs
@@ -8,11 +8,19 @@ namespace MainGameScripts.PlayableObjectsScripts
     public class HumanDebug : PlayableObject
     {
         public Rigidbody2D Rb;
-        private float speed = 5f;
+
+        private HumanController controller;
+
+        private void Start()
+        {
+            controller = this.GetComponent<HumanController>();
+        }
 
         public override void Move(Vector2 direction)
         {
-            Rb.velocity = new Vector2(direction.x, 0) * speed;
+            Rb.velocity = new Vector2(direction.x, 0) * controller.speed;
+            if (direction.x != 0)
+                controller.EyeDirection = (int) Mathf.Sign(direction.x);
         }
     }
 }
